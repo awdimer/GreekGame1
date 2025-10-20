706d1afcb544491f3e8340e7ec88bdf520c72ca3
Andreas Rist <ARist4523@student.pps.net>
2025-10-20 14:35:41 -0700

    Sign

--- Assets/Prop/sign.cs
+++ Assets/Prop/sign.cs
@@ -1,39 +1,39 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Sign : MonoBehaviour
-{
+public class SignHoverUI : MonoBehaviour{
     public GameObject textbox;
+    public Text textboxText;
+    public string message;
 
-    private void Start()
-    {
-        // Automatically find a child object named "Textbox"
-        if (textbox == null)
-        {
-            textbox = transform.Find("Textbox")?.gameObject;
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player") && textbox != null)
-        {
-            textbox.SetActive(true);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player") && textbox != null)
-        {
+    public string playerTag = "Player";
+    
+    private void Start(){
+        if(textbox != null){
             textbox.SetActive(false);
         }
     }
-
-    private void Update()
-    {
-        if (textbox != null && textbox.activeSelf)
-        {
-            textbox.transform.position = transform.position + new Vector3(0, 1.5f, 0);
+    private void OnTriggerEnter(Collider other){
+        if(other.CompareTag(playerTag)){
+            ShowTextbox();
+        }
+    }
+    private void OnTriggerExit(Collider other){
+        if(other.CompareTag(playerTag)){
+            HideTextbox();
+        }
+    }
+    void ShowTextbox(){
+        if(textbox != null){
+            textbox.SetActive(true);
+            if(textboxText != null){
+                textboxText.text = message;
+            }
+        }
+    }
+    void HideTextbox(){
+        if(textbox != null){
+            textbox.SetActive(false);
         }
     }
 }
