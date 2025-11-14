c5554ef8b9a724f636b5ba13c8e1bd731c74221a
cmiller7309 <158352640+cmiller7309@users.noreply.github.com>
2025-11-14 00:17:39 -0800

    lights, camera, assets
    added lighting with urp, made some level assets and changes the way the cam works, made turning to my mov script

--- Assets/Player/testmovemntplayer.cs
+++ Assets/Player/testmovemntplayer.cs
@@ -41,38 +41,38 @@ public class testPlayerMovement : MonoBehaviour
         moveInput.y = Input.GetAxisRaw("Vertical");
 
         Debug.Log(lastJumpTime);
-        if (isGrounded())
+        if (isGrounded())// coyote time
         {
             lastGroundedTime = coyoteTime;
             isJumping = false;
         }
-        if (Input.GetKeyUp(KeyCode.W)||Input.GetKeyUp(KeyCode.UpArrow))
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))// if the jump button is released the player will do a jump cut
         {
-            if(rb.linearVelocity.y > 0 && isJumping)
+            if (rb.linearVelocity.y > 0 && isJumping)
             {
-                rb.AddForce(Vector2.down * rb.linearVelocity.y*(1-jumpCutMultiplier), ForceMode2D.Impulse);
+                rb.AddForce(Vector2.down * rb.linearVelocity.y * (1 - jumpCutMultiplier), ForceMode2D.Impulse);
             }
         }
 
     }
     void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)){
+        
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)){// if trying to jump starts a countdown and if the player is grounded during the timer it will start the jump
             lastJumpTime = jumpBufferTime;
         }
-        if (lastJumpTime>0 && !isJumping && lastGroundedTime>0 )
+        if (lastJumpTime>0 && !isJumping && lastGroundedTime>0 )//check if the player can and wants to jump and runs jump
             {
                 jump();
                 Debug.Log("jumping");
             }
-
+        //start of left right movement
         float targetSpeed = moveInput.x * moveSpeed;
         float SpeedDif = targetSpeed - rb.linearVelocity.x;
         float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
         float movement = Mathf.Pow(Mathf.Abs(SpeedDif) * accelRate, velPower) * Mathf.Sign(SpeedDif);
         rb.AddForce(movement * Vector2.right);
-
-
+        //end of left right movement
 
 
 
@@ -84,24 +84,35 @@ public class testPlayerMovement : MonoBehaviour
         }
 
 
+            TurnCheck();
+        
+
+
     }
-    private bool isGrounded()
+    private bool isGrounded() //checks if the player is on the ground
     {
 
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
         return raycastHit.collider != null;
     }
-    private void jump()
+    private void jump() // applys jump force
     {
         isJumping = true;
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 
     }
-    private void TurnCheck()
+    private void TurnCheck() // checks if the player needs to turn
     {
-
+        if (moveInput.x > 0 && !isFacingRight)
+        {
+            Turn();
+        }
+        else if (moveInput.x < 0 && isFacingRight)
+        {
+            Turn();
+        }
     }
-    private void Turn()
+    private void Turn() //turns the player
     {
         if (isFacingRight)
         {
