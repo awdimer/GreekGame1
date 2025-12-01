a5e7db597f9b08a5af8c3ab1a355b11adf399967
Andreas Rist <ARist4523@student.pps.net>
2025-12-01 15:29:23 -0800

    Inventory
    I did some inventory stuff, I'm too tired to do more

--- Assets/UI/Inventory/ItemDatabase.cs
+++ Assets/UI/Inventory/ItemDatabase.cs
@@ -6,32 +6,58 @@ using System.IO;
 public class ItemDatabase : MonoBehaviour
 {
     private List<Item> database = new List<Item>();
-    private JsonData itemData;
-
+    JsonData itemData;
     void Start()
     {
-        //Item item = new Item(0, "ball", 5);
-        //database.Add(item);
-        //Debug.Log(database[0].Title); 
         itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
+        ConstructItemDatabase();
+        Debug.Log(database[1].Power);
+        Debug.Log(FetchItemByID(0).Description);
+    }
+    public Item FetchItemByID(int id)
+    {
+        for(int i = 0; i < database.Count; i++)
+            if(database[i].ID == id)
+            return database[i];
+        return null;
     }
     void ConstructItemDatabase()
     {
-        for (int i = 0; i < itemData.Count; i++)
+        for(int i = 0; i < itemData.Count; i++)
         {
-            //database.Add(new Item((int)itemData[i]["id"], itemData[i]["title"].ToString,itemData[i]["value"]));
+            database.Add(new Item((int)itemData[i]["id"], itemData[i]["title"].ToString(), (int)itemData[i]["value"],
+            (int)itemData[i]["stats"]["power"], (int)itemData[i]["stats"]["defence"], (int)itemData[i]["stats"]["vitality"], 
+            itemData[i]["description"].ToString(), (bool)itemData[i]["stackable"], (int)itemData[i]["rarity"], itemData[i]["slug"].ToString()));
         }
     }
 }
 public class Item
 {
+    
     public int ID { get; set; }
     public string Title { get; set; }
     public int Value { get; set; }
-    public Item(int id, string title, int value)
+    public int Power { get; set; }
+    public int Defence { get; set; }
+    public int Vitality { get; set; }
+    public string Description { get; set; }
+    public bool Stackable { get; set; }
+    public int Rarity { get; set; }
+    public string Slug { get; set; }
+    public Item(int id, string title, int value, int power, int defence, int vitality, string description, bool stackable, int rarity, string slug)
     {
-          this.ID = id; 
-          this.Title = title;
-          this.Value = value;
+        this.ID = id;
+        this.Title = title;
+        this.Value = value;
+        this.Power = power;
+        this.Vitality = vitality;
+        this.Description = description;
+        this.Stackable = stackable;
+        this.Rarity = rarity;
+        this.Slug = slug;
+    }
+    public Item()
+    {
+        this.ID = -1;
     }
 }
