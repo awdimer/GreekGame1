6c2cb8ded1fd0d8c22278e54afdf96b39a62a68a
colin <cmiller7309@student.pps.net>
2025-12-01 15:29:15 -0800

    wall jump in test

--- Assets/Player/testmovemntplayer.cs
+++ Assets/Player/testmovemntplayer.cs
@@ -13,13 +13,13 @@ public class testPlayerMovement : MonoBehaviour
     [SerializeField] private float decceleration;
     [SerializeField] private float frictionAmount;
     [SerializeField] private float jumpForce;
-    [SerializeField] private float wallJumpPowerY;
-    [SerializeField] private float wallJumpPowerX;
+    [SerializeField] private float wallJumpForce;
     [SerializeField] private float jumpBufferTime;
     [SerializeField] private float coyoteTime;
     [SerializeField] private float jumpCutMultiplier;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private Transform wallCheck;
     [SerializeField] public float KBforce; 
     [SerializeField] public float KBcounter;
     [SerializeField] public float KBtotaltime;
@@ -28,8 +28,8 @@ public class testPlayerMovement : MonoBehaviour
     private float lastGroundedTime;
     private float lastJumpTime;
     private bool isJumping;
-    private bool isFacingRight;
-    private bool isWallRight = false;
+    private bool isFacingRight = true;
+    private bool isOnWall;
 
     private Vector2 moveInput;
 
@@ -59,9 +59,8 @@ public class testPlayerMovement : MonoBehaviour
             if (rb.linearVelocity.y > 0 && isJumping)
             {
                 rb.AddForce(Vector2.down * rb.linearVelocity.y * (1 - jumpCutMultiplier), ForceMode2D.Impulse);
-            }
+           }
         }
-
     }
     void FixedUpdate()
     {
@@ -77,13 +76,15 @@ public class testPlayerMovement : MonoBehaviour
         float targetSpeed = moveInput.x * moveSpeed;
         float SpeedDif = targetSpeed - rb.linearVelocity.x;
         float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
+        float deccelRate = decceleration;
         float movement = Mathf.Pow(Mathf.Abs(SpeedDif) * accelRate, velPower) * Mathf.Sign(SpeedDif);
         rb.AddForce(movement * Vector2.right);
+        
         //end of left right movement
 
 
 
-        if (lastGroundedTime > 0 && Mathf.Abs(moveInput.x) < 0.01f)
+        if (isGrounded() && Mathf.Abs(moveInput.x) < 0.01f)
         {
             float amount = Mathf.Min(Mathf.Abs(rb.linearVelocity.x), Mathf.Abs(frictionAmount));
             amount *= Mathf.Sign(rb.linearVelocity.x);
@@ -109,7 +110,7 @@ public class testPlayerMovement : MonoBehaviour
         }
         
     
-
+        Debug.Log(rb.linearVelocity.x);
     }
     private bool isGrounded() //checks if the player is on the ground
     {
@@ -151,54 +152,25 @@ public class testPlayerMovement : MonoBehaviour
     }
     private bool onWall()
     {
-        Debug.Log(isWallRight);
-        bool facingRight = transform.localScale.x > 0;
-        if (facingRight)
-        {
-            RaycastHit2D hitRight = Physics2D.BoxCast(
-                boxCollider.bounds.center,
-                boxCollider.bounds.size,
-                0,
-                Vector2.right,
-                0.1f,
-                wallLayer
-            );
-
-            isWallRight = hitRight.collider != null;
-
-            return isWallRight;
-        }
-        else
-        {
-            RaycastHit2D hitLeft = Physics2D.BoxCast(
-                boxCollider.bounds.center,
-                boxCollider.bounds.size,
-                0,
-                Vector2.left,
-                0.1f,
-                wallLayer
-            );
-
-            isWallRight = hitLeft.collider != null;
-            isWallRight = false;  // explicitly false, since facing left
-
-            return isWallRight;
-        }
+        
+       return Physics2D.OverlapCircle(wallCheck.position, .02f,wallLayer);
     }
     private void wallJump()
     {
-        if (!isWallRight && wallJumpCooldown > 0.2f)
+        if (onWall() && !isGrounded())
         {
-            // Jump right and up if on left wall
-            rb.linearVelocity = new Vector2(wallJumpPowerX, wallJumpPowerY);
+            isJumping = true;
+            rb.AddForce(Vector2.up * wallJumpForce, ForceMode2D.Impulse);
+            if (isFacingRight)
+            {
+                rb.AddForce(Vector2.left * 2* wallJumpForce, ForceMode2D.Impulse);
+            }
+            if (!isFacingRight)
+            {
+                rb.AddForce(Vector2.right * wallJumpForce, ForceMode2D.Impulse);
+            }
+            
         }
-        else if (isWallRight && wallJumpCooldown > 0.2f)
-        {
-            // Jump left and up if on right wall
-            rb.linearVelocity = new Vector2(-wallJumpPowerX, wallJumpPowerY);
-
-        }
-        wallJumpCooldown = 0;
     }
 
 
