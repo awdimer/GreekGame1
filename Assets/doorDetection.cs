3bf9bed84afaaf4624e820369d2354bd2e7c6919
Adam Widmer <awidmer7246@student.pps.net>
2025-12-19 15:19:47 -0800

    one last one for real

--- Assets/doorDetection.cs
+++ Assets/doorDetection.cs
@@ -5,6 +5,7 @@ public class doorDetection : MonoBehaviour
     private BoxCollider2D boxCollider;
     [SerializeField] private GameObject doorSpawnPoint;
     [SerializeField] private GameObject Door;
+    private bool doorSpawned = false;
     void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
@@ -17,9 +18,10 @@ public class doorDetection : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.CompareTag("Player"))
+        if (collider.CompareTag("Player") && doorSpawned == false)
         {
             Instantiate(Door, doorSpawnPoint.transform.position, Quaternion.identity);
+            doorSpawned = true;
         }
     }
 }
