c6c92c1b1c35ce6acfc14797c9a91ad1f6e95b8d
Andreas Rist <ARist4523@student.pps.net>
2026-01-16 14:32:28 -0800

    Inventory Button Toggleable

--- Assets/UI/InventoryYesNo.cs
+++ Assets/UI/InventoryYesNo.cs
@@ -6,23 +6,9 @@ public class InventoryYesNo : MonoBehaviour
 {
     public GameObject InventoryPanel;
     public static bool isOpen = true;
-    //Start is called before the first frame update
-    void Start()
+public void ToggleInventory()
     {
-        InventoryPanel.SetActive(true);
-        isOpen = true;
-    }
-    //Update is called once per frame
-
-    public void InventoryOn()
-    {
-        if(isOpen = false)
-        {
-            
-        }
-    }
-    public void InventoryOff()
-    {
-        
+        isOpen = !isOpen;
+        InventoryPanel.SetActive(isOpen);
     }
 }
