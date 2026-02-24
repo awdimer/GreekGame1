07afad26437225b0137b493494cccea7dec49948
Andreas Rist <ARist4523@student.pps.net>
2026-02-24 15:29:22 -0800

    idk

---
+++ Assets/MoveCheckpoint.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCheckpoint : MonoBehaviour
+{
+    private RespawnScript respawn;
+    void Awake()
+    {
+        respawn = GameObject.FindGameObjectWithTag("Respawn").GetComponent<RespawnScript>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            respawn.respawnPoint = this.gameObject;
+        }
+    }
+}
