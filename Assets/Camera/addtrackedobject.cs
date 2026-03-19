f0e8a631aac1ef64d833a9f5cf7bb900384f4cb2
colin <cmiller7309@student.pps.net>
2026-03-19 15:31:01 -0700

    work on boss camera

--- Assets/Camera/addtrackedobject.cs
+++ Assets/Camera/addtrackedobject.cs
@@ -1,26 +1,19 @@
 using UnityEngine;
 using Unity.Cinemachine;
-using System.Linq.Expressions;
 
 
 
 public class addtrackedobject : MonoBehaviour
 {
-    void Awake()
-    {
-        GameObject[] boss = GameObject.FindGameObjectsWithTag("Boss");
-    }
+
     void Start()
     {    
-        GetComponent<CinemachineTargetGroup>().AddMember(transform, 1,1);
+       this.gameObject.GetComponent<CinemachineTargetGroup>().AddMember(transform, 1,1);
     } 
 
 
     void Update()
     {
-        //if(boss == null)
-       // {
-          //  GetComponent<CinemachineTargetGroup>().RemoveMember(transform);
-        //}
+    
     }
 }
