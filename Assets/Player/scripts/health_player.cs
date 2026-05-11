95cef4665c8b8e044626ea001a26b2c0d3328a8b
cmiller7309 <158352640+cmiller7309@users.noreply.github.com>
2026-05-10 22:19:20 -0700

    boss assets, level assets, player assests, and 2nd attack

---
+++ Assets/Player/scripts/health_player.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health_player : MonoBehaviour
+{
+    public static event Action OnPlayerDeath;
+    private testPlayerMovement TestPlayerMovement;
+    public int maxHealth = 100;
+    public int health;
+    [SerializeField] private healthbar playerhealthbar; 
+
+
+    void Start()
+    {
+        health = maxHealth;
+        TestPlayerMovement = GetComponent<testPlayerMovement>();
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+        if (TestPlayerMovement.isDodging)
+        {
+            Debug.Log("Player Dodged!");
+            return; // exit before damage is applied
+        }
+
+        health -= damage;
+
+        if (health <= 0)
+        {
+            
+            //Destroy(gameObject);
+            OnPlayerDeath?.Invoke();
+            health = maxHealth;
+        }
+        playerhealthbar.UpdateHealthBar(health,maxHealth);
+    }
+}
